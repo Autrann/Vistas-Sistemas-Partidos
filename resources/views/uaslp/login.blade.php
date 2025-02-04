7035fbfde787148cd8082aed54d0f9ed76f8492b
Autrann <luis.autran2@gmail.com>
2025-02-04 11:53:41 -0600

    fix: Banner en el login

--- resources/views/uaslp/login.blade.php
+++ resources/views/uaslp/login.blade.php
@@ -5,6 +5,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+     <!-- Favicon -->
+     <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}">
+
+     <!-- Bootstrap -->
+     <link href="{{ asset('content/bootstrap.min.css') }}" rel="stylesheet">
+     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+ 
+     <!-- Font Awesome -->
+     <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+ 
+     <!-- Estilos personalizados -->
+     <link href="{{ asset('content/OwnStyles/fuentes.css') }}" rel="stylesheet">
+     <link href="{{ asset('content/OwnStyles/Site.css') }}" rel="stylesheet">
+ 
+     <!-- jQuery -->
+     <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+ 
+     <script type="text/javascript">
+ 
+         $(function () {
+             window.onscroll = function () { scrollFunction() };
+         });
+     
+     
+         function scrollFunction() {
+             if ($(window).scrollTop() > 100) {
+                 $(".logoUASLP").removeClass("d-block").addClass("d-none");
+                 $(".textoUASLP").removeClass("d-none").addClass("d-block");
+                 $(".divisorUASLP-ENTIDAD").removeClass("d-block").addClass("d-none");
+                 $(".divisorUASLP-ENTIDADScroll").removeClass("d-none").addClass("d-block");
+                 $(".header").css("height", "85px");
+     
+             }
+             if ($(window).scrollTop() < 40) {
+                 $(".logoUASLP").removeClass("d-none").addClass("d-block");
+                 $(".textoUASLP").removeClass("d-block").addClass("d-none");
+                 $(".divisorUASLP-ENTIDAD").removeClass("d-none").addClass("d-block");
+                 $(".divisorUASLP-ENTIDADScroll").removeClass("d-block").addClass("d-none");
+                 $(".header").css("height", "120px");
+             }
+         }
+     </script>
     <style>
         .login-container {
             max-width: 300px;
@@ -24,6 +68,7 @@
 <body>
     <!-- Header -->
     @include('components.banner')
+
 
     <!-- Login -->
     <div class="container mt-5">
